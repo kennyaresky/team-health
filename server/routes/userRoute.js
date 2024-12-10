21e5bceda52f7f582ec4cb22999a952b6d7a325b
NelsonHennessiAyodeji <nelsonhennessiayodeji@gmail.com>
2024-12-10 14:07:56 +0100

    server cleanup

--- server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsers, createUser } = require("../controllers/userController");
-
-router.route("/").get(getAllUsers).post(createUser);
-
-module.exports = router;
